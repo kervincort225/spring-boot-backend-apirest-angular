9e66aca95e5f092eb95fa7d065fce4d71aee74af
kervin cortez <kervin.cortez@pad.cl>
2020-10-30 14:29:09 -0300

    iniciar el proyecto backend de angular

---
+++ src/main/java/com/bolsasdeideas/springboot/backend/apirest/SpringBootBackendApirestAngularApplication.java
@@ -0,0 +1,13 @@
+package com.bolsasdeideas.springboot.backend.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendApirestAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBackendApirestAngularApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bolsasdeideas/springboot/backend/apirest/SpringBootBackendApirestAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolsasdeideas.springboot.backend.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendApirestAngularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
