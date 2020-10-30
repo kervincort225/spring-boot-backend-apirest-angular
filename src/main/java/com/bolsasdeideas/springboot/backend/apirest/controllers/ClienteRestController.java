bfbc0e824ef04959e1e2e6622ef5ae146e7ee7f2
kervin cortez <kervin.cortez@pad.cl>
2020-10-30 17:39:12 -0300

    uso de cross para compartir  recursos en api rest

--- src/main/java/com/bolsasdeideas/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/bolsasdeideas/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -4,12 +4,14 @@ import com.bolsasdeideas.springboot.backend.apirest.models.entity.Cliente;
 import com.bolsasdeideas.springboot.backend.apirest.models.services.ClienteServiceImpl;
 import com.bolsasdeideas.springboot.backend.apirest.models.services.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins={"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
